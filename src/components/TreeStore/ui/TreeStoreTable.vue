4c2c41bbc50b2102b94b514ba4c15f633c8a5e46
timurtapeev <tspeevtimur@gmail.com>
2026-03-30 15:47:37 +0500

    feat: Создан таблица для отображения дерева

---
+++ src/components/TreeStore/ui/TreeStoreTable.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { TreeStore } from '@/components/TreeStore/model/treeStore.ts'
+import { AgGridVue } from 'ag-grid-vue3'
+import { makeTree } from '@/components/TreeStore/lib/makeTree.ts'
+import type { TreeStoreData } from '@/components/TreeStore/types/treeStoreData.ts'
+
+const props = defineProps<{
+  list: TreeStoreData[]
+}>()
+
+const store = new TreeStore(props.list)
+const value = store.getAll()
+
+const gridOptions = {
+  treeData: true,
+  treeDataChildrenField: 'children',
+  rowData: makeTree(value),
+  columnDefs: [
+    {
+      field: 'label',
+      headerName: 'Имя',
+    },
+  ],
+  autoGroupColumnDef: {
+    headerName: 'Категория',
+    field: 'type',
+    cellRendererParams: {
+      suppressCount: true,
+    },
+  },
+  groupDefaultExpanded: -1,
+  defaultColDef: {
+    flex: 1,
+  },
+  rowNumbers: true,
+}
+</script>
+
+<template>
+  <AgGridVue :gridOptions="gridOptions" style="height: 500px; width: 1000px" />
+</template>
+
+<style scoped></style>
