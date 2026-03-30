de5b7e36168f5913ddf990aad95fee220e86b5e8
timurtapeev <tspeevtimur@gmail.com>
2026-03-30 14:30:56 +0500

    feat: Установлена библиотека ag grid

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+import { TreeDataModule, RowNumbersModule } from 'ag-grid-enterprise'
+
+ModuleRegistry.registerModules([AllCommunityModule, TreeDataModule, RowNumbersModule])
 
 createApp(App).mount('#app')
